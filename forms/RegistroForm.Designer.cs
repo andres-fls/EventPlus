065c6e578ad1acd3103df66b2a6d9a920184a7fe
leoneldavidalvarez1-dot <leoneldavidalvarez1@gmail.com>
2026-05-08 00:53:56 -0500

    Corregir tipo de letra a la caja de texto comfirmar contraseña

--- forms/RegistroForm.Designer.cs
+++ forms/RegistroForm.Designer.cs
@@ -109,6 +109,7 @@
             // 
             // txtConfirmarPassword
             // 
+            this.txtConfirmarPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtConfirmarPassword.Location = new System.Drawing.Point(174, 269);
             this.txtConfirmarPassword.Name = "txtConfirmarPassword";
             this.txtConfirmarPassword.Size = new System.Drawing.Size(282, 26);
